d99fe55f92ced1b02380081c3ef9faecd17461cd
Oliver Elfverson <oliver@zyner.org>
2026-04-03 15:22:46 +0200

    Update to use "Vue Router" instead of "unplugin-vue-router"

--- src/router/index.js
+++ src/router/index.js
@@ -6,7 +6,7 @@
 
 // Composables
 import { setupLayouts } from "virtual:generated-layouts";
-import { createRouter, createWebHistory } from "vue-router";
+import { createRouter, createWebHistory } from "vue-router/auto";
 import { routes } from "vue-router/auto-routes";
 
 const router = createRouter({
