6311336598923bf1d0dc576703bcd30db951dd20
Elias Sjögreen <eliassjogreen1@gmail.com>
2026-07-24 11:22:32 +0200

    feat: Add and configure storybook

---
+++ src/components/Hero.stories.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+import Hero from "./Hero.vue";
+</script>
+
+<template>
+  <Stories title="Hero" :component="Hero">
+    <Story title="Primary">
+      <Hero />
+    </Story>
+  </Stories>
+</template>
