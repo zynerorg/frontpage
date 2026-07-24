0f32fe5ef12c5f479dcfa589d61399d49d040897
Elias Sjögreen <eliassjogreen1@gmail.com>
2026-07-24 10:07:27 +0200

    chore: Delete old frontpage

--- src/components/AppSystemBar.vue
+++
@@ -1,8 +0,0 @@
-<template>
-  <v-system-bar class="justify-center">
-    <strong>Zyner 2025</strong> — {{ $t('systembar.reconstruction') }}
-    {{
-      !$vuetify.display.mobile ? $t('systembar.not_opened') : null
-    }}
-  </v-system-bar>
-</template>
